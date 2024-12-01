68d4b1b5853258ce2efa800d5e97dd432e0d0277
Defaktor <defaktor@gmail.com>
2024-12-01 13:33:09 +0300

    Implement mongo-sharding

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-###
-# Инициализируем бд
-###
-
-docker compose exec -T mongodb1 mongosh <<EOF
-use somedb
-for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+docker exec -i configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+)
+exit()
 EOF
 
+docker exec -i shard1 mongosh --port 27018 <<EOF
+rs.initiate(
+  {
+    _id : "shard1",
+    members: [
+      { _id : 0, host : "shard1:27018" },
+     // { _id : 1, host : "shard2:27019" }
+    ]
+  }
+)
+exit()
+EOF
+
+docker exec -i shard2 mongosh --port 27019 <<EOF
+rs.initiate(
+  {
+    _id : "shard2",
+    members: [
+     // { _id : 0, host : "shard1:27018" },
+      { _id : 1, host : "shard2:27019" }
+    ]
+  }
+)
+exit()
+EOF
